4b85e4a01233b667f8ba7abd4be752e494446a95
Ángel Castañeda <angelaxel1420@disroot.org>
2023-09-14 04:31:03 -0500

    Add perl command for target=_blank

--- TeX2HTML.sh
+++ TeX2HTML.sh
@@ -6,5 +6,9 @@ cp "$file" temp.tex
 sed -i 's/\\hangindent=\\parindent//' temp.tex # remove equals sign
   
 pandoc -f latex -t html -s -o main.html temp.tex
+
+# makes http & https links have target _blank and rel noopener noreferrer
+perl -i -0pe 's/(<\W*a\W*[^>]*href=)(["'"'"']http[s]?:\/\/[^"'"'"'>]*["'"'"'])([^>]*>)/$1$2 target="_blank" rel="noopener noreferrer"$3/g' main.html
+
 rm -v temp.tex
 firefox main.html
